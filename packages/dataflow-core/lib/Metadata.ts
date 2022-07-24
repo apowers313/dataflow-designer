328d41e543b35bdc89939e5f0f700882adc0c483
Adam Powers <apowers@ato.ms>
2022-07-24 10:27:05 -0700

    add docs, delint

--- packages/dataflow-core/lib/Metadata.ts
+++ packages/dataflow-core/lib/Metadata.ts
@@ -1,16 +1,34 @@
+/**
+ * An abstract class for representing all types of metadata
+ */
 export abstract class MetadataType {
     abstract name: string;
     abstract value: unknown;
 }
 
 export type MetadataTypeConstructor = new () => MetadataType;
+
+/**
+ * A collection of metadata elements
+ */
 export class MetadataCollection {
     #collection: Array<MetadataType> = [];
 
-    add(type: MetadataType): void {
-        this.#collection.push(type);
+    /**
+     * Adds metadata to the collection
+     *
+     * @param md - The metadata to add to the collection
+     */
+    add(md: MetadataType): void {
+        this.#collection.push(md);
     }
 
+    /**
+     * Returns the first instance of the requested type, or null if it's not found
+     *
+     * @param type - The type to find, based on the constructor for the type
+     * @returns The first instance of the type that was added to the collection, or null if no instance was found.
+     */
     get<T extends MetadataTypeConstructor>(type: T): InstanceType<T> | null {
         for (let i = 0; i < this.#collection.length; i++) {
             const item = this.#collection[i];
@@ -22,71 +40,36 @@ export class MetadataCollection {
         return null;
     }
 
+    /**
+     * Returns true if the collection has at least one instance of matching metadata, false otherwise
+     *
+     * @param type - The type to find, based on the constructor for the type
+     * @returns True if the collection contains the specified type, false otherwise
+     */
     has<T extends MetadataTypeConstructor>(type: T): boolean {
         return !!this.get(type);
     }
 
+    /**
+     * The number of members in the collection
+     */
     get size(): number {
         return this.#collection.length;
     }
 }
 
+/**
+ * A metadata element indicating that the stream has successfully completed
+ */
 export class DataflowEnd extends MetadataType {
     name = "dataflow-end";
     value = "end";
-
-    print(): void {
-        console.log("the end.");
-    }
 }
 
+/**
+ * A metadata element indicating that the stream has successfully started
+ */
 export class DataflowStart extends MetadataType {
     name = "dataflow-start";
     value = "start";
 }
-
-// export abstract class MetadataType<Value> {
-//     abstract readonly namespace: string;
-//     abstract readonly type: string;
-//     abstract value: Value;
-
-//     // is<T extends MetadataType<Value>>(): this is T {
-//     //     console.log("this.constructor", this.constructor);
-//     //     console.log("this.constructor.name", this.constructor.name);
-//     //     return true;
-//     // }
-//     is(ns: string, t: string): this is typeof this {
-//         console.log("this.namespace", this.namespace, ns);
-//         console.log("this.type", this.type, t);
-//         return this.namespace === ns && this.type === t;
-//     }
-
-//     get(): Value {
-//         return this.value;
-//     }
-
-//     set(v: Value): void {
-//         this.value = v;
-//     }
-// }
-
-// type TypeRegistry = Map<string, MetadataType<unknown>>;
-// const metadataRegistry: Map<string, TypeRegistry> = new Map();
-
-// export class Metadata {
-//     static register(newType: MetadataType<unknown>): void {
-//         let tr = metadataRegistry.get(newType.namespace);
-
-//         if (!tr) {
-//             tr = new Map();
-//             metadataRegistry.set(newType.namespace, tr);
-//         }
-
-//         if (tr.has(newType.type)) {
-//             throw new Error(`type already registered: ${newType.type}`);
-//         }
-
-//         tr.set(newType.type, newType);
-//     }
-// }
-
