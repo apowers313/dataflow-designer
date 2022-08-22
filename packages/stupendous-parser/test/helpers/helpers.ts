eb8abe51bfcfbc4af7b820ecf4023cddc0c086f0
Adam Powers <apowers@ato.ms>
2022-08-21 21:57:32 -0700

    add timeout helper

--- packages/stupendous-parser/test/helpers/helpers.ts
+++ packages/stupendous-parser/test/helpers/helpers.ts
@@ -1,4 +1,10 @@
 /* eslint-disable jsdoc/require-jsdoc */
+export async function timeout(ms: number): Promise<void> {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
 export function objectStream(objs: Array<Record<any, any>>): ReadableStream {
     let curr = 0;
     return new ReadableStream({
