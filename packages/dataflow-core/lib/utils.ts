bb7daa1cf9a7421b19df27438c896d3323e949d3
Adam Powers <apowers@ato.ms>
2022-09-04 09:39:16 -0700

    add interlock

--- packages/dataflow-core/lib/utils.ts
+++ packages/dataflow-core/lib/utils.ts
@@ -140,3 +140,38 @@ export const inspectSymbol = Symbol.for("nodejs.util.inspect.custom");
 export class DataflowError extends Error {}
 
 export type ContextConstructor<T> = new (... args: any[]) => T;
+
+type ResolveFn = (... args: any[]) => void;
+
+export class Interlock<T> {
+    #writeDone!: Promise<T>;
+    #writeResolve!: ResolveFn;
+    #readDone!: Promise<void>;
+    #readResolve!: ResolveFn;
+
+    constructor() {
+        this.reset();
+    }
+
+    async send(data: T | null): Promise<void> {
+        this.#writeResolve(data);
+        await this.#readDone;
+    }
+
+    async recv(): Promise<T | null> {
+        const data = await this.#writeDone;
+        this.#readResolve();
+        this.reset();
+        return data;
+    }
+
+    reset(): void {
+        this.#writeDone = new Promise((resolve) => {
+            this.#writeResolve = resolve;
+        });
+
+        this.#readDone = new Promise((resolve) => {
+            this.#readResolve = resolve;
+        });
+    }
+}
