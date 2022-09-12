3b2c1fd150894056df87a0c63c292f1124a657de
Adam Powers <apowers@ato.ms>
2022-09-11 21:00:40 -0700

    add helpers to template

--- .template/test/_.test.ts
+++ .template/test/_.test.ts
@@ -1,6 +1,8 @@
 import { {{fclass}} } from "../index";
 import { assert } from "chai";
+// import { helpers } from "@dataflow-designer/dataflow-core";
 // import { spy } from "sinon";
+// const { TestSource } = helpers;
 
 describe("{{fclass}}", function () {
     it("is function", function () {
