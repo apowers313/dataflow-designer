88079ca5fb2da00e6d5414afd758623603a9906f
Adam Powers <apowers@ato.ms>
2022-09-11 22:04:57 -0700

    add sugar to templates

--- .template/lib/_.ts
+++ .template/lib/_.ts
@@ -1,6 +1,19 @@
-import {Source} from "@dataflow-designer/dataflow-core";
+import {Chunk, Through, ThroughOpts, ThroughMethods} from "@dataflow-designer/dataflow-core";
+
+interface {{fclass}}Opts extends Omit<ThroughOpts, "through"> {}
 
 /**
  * Just testing
  */
-export class {{fclass}} { }
+export class {{fclass}} {
+    constructor(opts: {{fclass}}Opts = {}) {
+        super({
+            ... opts,
+            through: (chunk, methods) => this.#through(chunk, methods),
+        })
+    }
+
+    #through(chunk: Chunk, methods: ThroughMethods) {
+        methods.send(0, chunk);
+    }
+}
