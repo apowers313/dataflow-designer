b673185f3ad95da1d00e73c97b1b907ea350ab52
Adam Powers <apowers@ato.ms>
2022-09-28 21:38:00 -0700

    update docker build and publish

--- .createmanifest.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -x
-
-ls -d packages/node-red-dataflow* | cut -d/ -f2 > docker/assets/npm_manifest
---
+++ docker/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# make sure our CWD is this directory
+cd "$(dirname "$0")"
+
+# get the lerna version
+VERSION=`npx jq2 "$.version" ../lerna.json`
+PACKAGE="apowers313/dataflow-designer"
+LATEST_PACKAGE=$PACKAGE:latest
+VERSION_PACKAGE=$PACKAGE:v$VERSION
+
+if [ "$1" = "build" ]; then
+    ls -d ../packages/node-red-dataflow* | cut -d/ -f3 > assets/npm_manifest
+    docker build --no-cache --tag $LATEST_PACKAGE --tag $VERSION_PACKAGE .
+elif [ "$1" = "publish" ]; then
+    docker push $LATEST_PACKAGE
+    docker push $VERSION_PACKAGE
+else
+    echo "usage: $0 <build|publish>"
+fi
