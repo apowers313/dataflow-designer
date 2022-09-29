39cb053d45c235a19a85ae3f1c208b4b57221324
Adam Powers <apowers@ato.ms>
2022-09-28 23:29:03 -0700

    refactor docker build

--- docker/build.sh
+++ docker/build.sh
@@ -14,8 +14,12 @@ if [ "$1" = "build" ]; then
     docker build --no-cache --tag $LATEST_PACKAGE --tag $VERSION_PACKAGE .
 elif [ "$1" = "publish" ]; then
     docker login
-    docker push $LATEST_PACKAGE
     docker push $VERSION_PACKAGE
+    docker push $LATEST_PACKAGE
+elif [ "$1" = "run" ]; then
+    docker run -it -p 1880:1880 $LATEST_PACKAGE
+elif [ "$1" = "shell" ]; then
+    docker run -it -p 1880:1880 --entrypoint sh $LATEST_PACKAGE
 else
-    echo "usage: $0 <build|publish>"
+    echo "usage: $0 <build|publish|run|shell>"
 fi
